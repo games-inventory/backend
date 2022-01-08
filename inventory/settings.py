9ffde48150f2ba0b9d5ebcf3fe75417a2c65a8bf
Tan Yee Jian <yeejian.tan@u.nus.edu>
2022-01-08 08:15:46 +0800

    fixup! Merge remote-tracking branch 'origin/master'

--- inventory/settings.py
+++ inventory/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
     "gamesinventory",
     "corsheaders",
 ]
@@ -59,8 +58,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "inventory.urls"
